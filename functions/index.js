b885bbb32ccb1f832287a537208577332e1e462e
Firdausy Bashir <43407297+Firdausi1@users.noreply.github.com>
2021-01-16 13:13:59 +0100

    third

--- functions/index.js
+++ functions/index.js
@@ -23,9 +23,9 @@ const mailTransport = nodemailer.createTransport({
 })
 
 exports.submit = functions.https.onRequest((req, res) => {
-  res.set('Access-Control-Allow-Origin', '*')
-  res.set('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS')
-  res.set('Access-Control-Allow-Headers', '*')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', '*')
 
   if (req.method === 'OPTIONS') {
     res.end()
